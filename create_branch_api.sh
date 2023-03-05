0f6fdff18ad4bbc7dc61ac64e0aee5094ca34571
Orange-Lightning <90224411+redMatt4133@users.noreply.github.com>
2023-03-05 18:34:05 -0500

    Add create branch API script

---
+++ create_branch_api.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Using pre-defined Gitlab CI/CD variables - CI_SERVER_HOST, CI_PROJECT_ID
+# Create a project access token and store the token into a custom CI/CD variable named PROJECT_TOKEN
+curl --request POST --header "PRIVATE-TOKEN: ${PROJECT_TOKEN}" "https://${CI_SERVER_HOST}/api/v4/projects/${CI_PROJECT_ID}/branches?branch=${new_branch}&ref=${source_branch}"
